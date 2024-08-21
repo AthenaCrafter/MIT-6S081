29288395279de4117032b58082e868ccff4d7f38
HuYumu <Nanjinghym@163.com>
2024-08-21 11:41:46 +0800

    feat: networking, e1000_recv() using while to recv all avaliable buf and send them to kernel altogether

--- kernel/e1000.c
+++ kernel/e1000.c
@@ -20,6 +20,8 @@ static struct mbuf *rx_mbufs[RX_RING_SIZE];
 static volatile uint32 *regs;
 
 struct spinlock e1000_lock;
+struct spinlock e1000_rx_lock;
+struct spinlock e1000_tx_lock;
 
 // called by pci_init().
 // xregs is the memory address at which the
@@ -30,6 +32,8 @@ e1000_init(uint32 *xregs)
   int i;
 
   initlock(&e1000_lock, "e1000");
+  initlock(&e1000_rx_lock, "e1000_rx");
+  initlock(&e1000_tx_lock, "e1000_tx");
 
   regs = xregs;
 
@@ -102,7 +106,27 @@ e1000_transmit(struct mbuf *m)
   // the TX descriptor ring so that the e1000 sends it. Stash
   // a pointer so that it can be freed after sending.
   //
-  
+  acquire(&e1000_lock);
+
+  uint32 index = regs[E1000_TDT];
+
+  if((tx_ring[index].status & E1000_TXD_STAT_DD) == 0) {
+    printf("hasn't finished the corresponding previous transmission request %d\n", index);
+    release(&e1000_lock);
+    return -1;
+  }
+
+  if(tx_mbufs[index] != 0)
+    mbuffree(tx_mbufs[index]);
+
+  tx_mbufs[index] = m;
+  tx_ring[index].addr = (uint64)m->head;
+  tx_ring[index].length = m->len;
+  tx_ring[index].cmd = E1000_TXD_CMD_RS | E1000_TXD_CMD_EOP;
+
+  regs[E1000_TDT] = (regs[E1000_TDT] + 1) % TX_RING_SIZE;
+
+  release(&e1000_lock);
   return 0;
 }
 
@@ -115,6 +139,43 @@ e1000_recv(void)
   // Check for packets that have arrived from the e1000
   // Create and deliver an mbuf for each packet (using net_rx()).
   //
+
+  // while(1) {
+  //   uint32 index = (regs[E1000_RDT] + 1) % RX_RING_SIZE;
+
+  //   if((rx_ring[index].status & E1000_RXD_STAT_DD) == 0) {
+  //     return;
+  //   }
+
+  //   rx_mbufs[index]->len = rx_ring[index].length;
+  //   net_rx(rx_mbufs[index]);
+
+  //   rx_mbufs[index] = mbufalloc(0);
+  //   if(!rx_mbufs[index]) {
+  //     panic("e1000_recv");
+  //   }
+  //   rx_ring[index].addr = (uint64)rx_mbufs[index]->head;
+  //   rx_ring[index].status = 0;
+
+  //   regs[E1000_RDT] = (regs[E1000_RDT] + 1) % RX_RING_SIZE;
+  // }
+  uint32 index = (regs[E1000_RDT] + 1) % RX_RING_SIZE;
+
+  if((rx_ring[index].status & E1000_RXD_STAT_DD) == 0) {
+    return;
+  }
+
+  rx_mbufs[index]->len = rx_ring[index].length;
+  net_rx(rx_mbufs[index]);
+
+  rx_mbufs[index] = mbufalloc(0);
+  if(!rx_mbufs[index]) {
+    panic("e1000_recv");
+  }
+  rx_ring[index].addr = (uint64)rx_mbufs[index]->head;
+  rx_ring[index].status = 0;
+
+  regs[E1000_RDT] = (regs[E1000_RDT] + 1) % RX_RING_SIZE;
 }
 
 void
