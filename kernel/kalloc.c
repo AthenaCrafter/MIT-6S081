14de64b597b0fa5b9a7c1bd8fe841c493a5fa560
momo <Nanjinghym@163.com>
2025-09-12 13:06:15 +0800

    feat: Memory allocator

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -18,16 +18,31 @@ struct run {
   struct run *next;
 };
 
-struct {
+struct kmem_t {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+};
+
+struct kmem_t kmem[NCPU];
 
 void
 kinit()
-{
-  initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+{ 
+  char lock_name[16];
+  void *pa;
+  int i, page_per_cpu, left, page_cnt;
+
+  page_per_cpu = ((PHYSTOP - (uint64)end) / PGSIZE) / NCPU;
+  left = ((PHYSTOP - (uint64)end) / PGSIZE) % NCPU;
+  pa = (void*)end;
+  for (i = 0; i < NCPU; i++) {
+    snprintf(lock_name, 16, "kmem%d", i);
+    initlock(&kmem[i].lock, lock_name);
+
+    page_cnt = left-- > 0? page_per_cpu+1 : page_per_cpu;
+    freerange(pa, (void*)(pa + page_cnt * PGSIZE));
+    pa += page_cnt * PGSIZE;
+  }
 }
 
 void
@@ -46,6 +61,7 @@ freerange(void *pa_start, void *pa_end)
 void
 kfree(void *pa)
 {
+  int id;
   struct run *r;
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
@@ -54,12 +70,16 @@ kfree(void *pa)
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
+  push_off();
+  id = cpuid();
+  pop_off();
+
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  acquire(&kmem[id].lock);
+  r->next = kmem[id].freelist;
+  kmem[id].freelist = r;
+  release(&kmem[id].lock);
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -68,15 +88,36 @@ kfree(void *pa)
 void *
 kalloc(void)
 {
+  int i, id;
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+  push_off();
+  id = cpuid();
+  pop_off();
 
+  acquire(&kmem[id].lock);
+  r = kmem[id].freelist;
   if(r)
+    kmem[id].freelist = r->next;
+  release(&kmem[id].lock);
+
+  if(r) {
     memset((char*)r, 5, PGSIZE); // fill with junk
+    return (void*)r;
+  }
+
+  for(i = 0; i < NCPU; i++) {
+    if(i == id)
+      continue;
+    acquire(&kmem[i].lock);
+    r = kmem[i].freelist;
+    if(r) {
+      kmem[i].freelist = r->next;
+      release(&kmem[i].lock);
+      break;
+    }
+    release(&kmem[i].lock);
+  }
+
   return (void*)r;
 }
